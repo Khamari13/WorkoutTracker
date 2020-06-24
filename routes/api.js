79e6dfca36e7fba730a2487fbdc589376a649502
Khamari Thompson <61212885+Khamari13@users.noreply.github.com>
2020-06-24 16:24:04 -0400

    added my routes to the routes.js file

--- routes/api.js
+++ routes/api.js
@@ -11,3 +11,42 @@ router.post("/api/workouts", ({ body }, res) => {
         res.status(400).json(err);
     });
 });
+
+router.post("/workouts/bulk", ({ body }, res) => {
+    workout.insertMany(body)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+router.get("/api/workout/range", (req, res) => {
+    workout.find({})
+    
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+router.put('/api/workouts/:id', function ({body,params}, res) {
+    Workout.findByIdAndUpdate(
+      params.id,
+      { $push: { exercises: body } },
+      // "runValidators" will ensure new exercises meet our schema requirements
+      { new: true, runValidators: true }
+    )
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  })
+  
+  
+  module.exports = router;
